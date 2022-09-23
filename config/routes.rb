236d0ab3a1e41fc63bead3da869e11b542e0caf1
Muhammad motala <muhammad.motala@docfox.io>
2022-09-23 16:33:41 +0200

    end ch5 Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'users/new'
   root 'static_pages#home'
   # changed the below in 5.3.2 to follow the model below the comments
   # get 'static_pages/home'
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help' #,  # as:'helf'  use this to make an alternative name for the path
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'application#hello'
