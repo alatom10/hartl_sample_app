abbc63fc74f769841ebd2595bfc8f38d8233257f
Muhammad motala <muhammad.motala@docfox.io>
2022-09-27 15:49:15 +0200

    revert changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   # get 'users/new'
   root 'static_pages#home'
   # changed the below in 5.3.2 to follow the model below the comments
@@ -11,9 +10,6 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  get '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
 
