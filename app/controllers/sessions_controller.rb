8f81215539032b732a55716fc829344424705cc1
Muhammad motala <muhammad.motala@docfox.io>
2022-09-28 15:51:14 +0200

    complete exercise 9.3.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    #note in the exercise for 9.3.1 we make user an instance variable hence the @ in front of 'user'
+    @user = User.find_by(email: params[:session][:email].downcase)
     # if user && user.authenticate(params[:session][:password])
-    if user&.authenticate(params[:session][:password]) # this uses the safe navigation operator &. which is the same as the above
+    if @user&.authenticate(params[:session][:password]) # this uses the safe navigation operator &. which is the same as the above
           # Log the user in and redirect to the user's show page.
       reset_session
-      log_in user
-      redirect_to user
+      log_in @user
+      # remember user #removed in 9.2 to cater for remem me checkbox
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
         # Create an error message.
         # flash[:danger] = 'Invalid email/password combination' # Not quite right! - we use flash.now instead as flash does not consider render a new request but flash.now considers all and displays once
@@ -19,7 +22,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    # log_out
+    log_out if logged_in? #change in ch 9, p 523
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,12 +5,24 @@ module SessionsHelper
         session[:user_id] = user.id
     end
     
-    def current_user
-        if session[:user_id]
-           @current_user ||= User.find_by(id: session[:user_id]) #means if current user is null then do the find by from the db
-        end 
-    end
+    # def current_user
+    #     if session[:user_id]
+    #        @current_user ||= User.find_by(id: session[:user_id]) #means if current user is null then do the find by from the db
+    #     end 
+    # end
 
+    # Returns the user corresponding to the remember token cookie.
+  def current_user
+    if (user_id = session[:user_id]) # reads as if user_id in sessions is not null then assign it to user_id
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end 
+  end
 
   def logged_in?
     !current_user.nil?
@@ -18,8 +30,22 @@ module SessionsHelper
 
   # Logs out the current user. def log_out
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
   end
   
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id 
+    cookies.permanent[:remember_token] = user.remember_token
+  end    
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
 end
