eb6541c419aad87414f4c6eca3ed97159d5410be
Muhammad motala <muhammad.motala@docfox.io>
2022-09-29 15:27:34 +0200

    exercise 10.3.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,14 @@ class SessionsController < ApplicationController
     # if user && user.authenticate(params[:session][:password])
     if @user&.authenticate(params[:session][:password]) # this uses the safe navigation operator &. which is the same as the above
           # Log the user in and redirect to the user's show page.
+      forwarding_url = session[:forwarding_url]
       reset_session
       log_in @user
       # remember user #removed in 9.2 to cater for remem me checkbox
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       session[:session_token] = @user.session_token
-      redirect_to @user
+      # redirect_to @user changed in chp10 for friendly forwarding to the below;
+      redirect_to forwarding_url || @user
     else
         # Create an error message.
         # flash[:danger] = 'Invalid email/password combination' # Not quite right! - we use flash.now instead as flash does not consider render a new request but flash.now considers all and displays once
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update] #chapter 10.2.1
+  before_action :correct_user, only: [:edit, :update]
   
+  def index 
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     # debugger
@@ -13,7 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
+      # reset_session removed in chp 10 pg 558
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -24,12 +30,41 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])   pg 576 removed bacause the before_action :correct_user checks the correct user already
   end
 
+  def update
+    # @user = User.find(params[:id]) pg 576
+    if @user.update(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+  else
+      render 'edit'
+    end
+  end
 
   private
       def user_params
         params.require(:user).permit(:name, :email, :password,:password_confirmation)
+      end
+
+
+      # before filters
+          # Confirms a logged-in user.
+      def logged_in_user
+        unless logged_in?
+          store_location
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end 
+      end
+
+
+    # Confirms the correct user.
+      def correct_user
+        @user = User.find(params[:id]) 
+        # redirect_to(root_url) unless @user == current_user # replaced with below pg 579 due to current_user method created in the sessions helper
+        redirect_to(root_url) unless current_user?(@user)
       end
 end
