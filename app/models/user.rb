40bf4c517ed9587a405d93ce84cff459fb7e1b90
Muhammad motala <muhammad.motala@docfox.io>
2022-10-03 09:21:18 +0200

    complete chp 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
     # before_save { self.email = email.downcase }
-    before_save { email.downcase! } # alternartive way of writing the above
+    # before_save { email.downcase! } # alternartive way of writing the above
+    #  the above save was removed in 11.1 pg 629 and replaced with a method reference as below
+    attr_accessor :remember_token, :activation_token 
+    before_save :downcase_email
+    before_create :create_activation_digest
 
     validates :name, presence: true, length: { maximum: 50 }
     #note the above is the same as: validates(:name, prescence: true)
@@ -46,13 +49,47 @@ class User < ApplicationRecord
     end
 
     # Returns true if the given token matches the digest.
-    def authenticated?(remember_token) 
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token) #pg 511 for explanation
+    # def authenticated?(remember_token) 
+    #     return false if remember_digest.nil?
+    #     BCrypt::Password.new(remember_digest).is_password?(remember_token) #pg 511 for explanation
+    # end
+    # we replaced the above with a more generic method below:
+
+    # Returns true if the given token matches the digest. chapter 11.4 pg652
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
     end
+
 
     # Forgets a user.
     def forget 
         update_attribute(:remember_digest, nil)
     end
+
+    # Activates an account.
+    def activate
+        # update_attribute(:activated,    true)
+        # update_attribute(:activated_at, Time.zone.now) 
+        # the above can be done in 1 line to prevent multiple db calls
+        update_columns(activated: true, activated_at: Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email 
+        UserMailer.account_activation(self).deliver_now
+    end
+
+    private
+    # Converts email to all lower-case.
+        def downcase_email
+            self.email = email.downcase
+        end
+
+    # Creates and assigns the activation token and digest.
+        def create_activation_digest
+            self.activation_token = User.new_token 
+            self.activation_digest = User.digest(activation_token)
+        end
 end
